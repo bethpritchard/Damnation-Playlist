cc7c92628fc46897b55460b9be24fc483a9cc608
Beth <bethpritchard97@gmail.com>
2021-02-27 09:37:42 +0000

    initial commit

---
+++ playlist_generator.py
@@ -0,0 +1,20 @@
+# TODO: scrape website
+
+# TODO: store bands in list
+
+# TODO: check for bands in file
+
+#TODO: store bands in file
+
+
+# TODO: spotify creds
+
+
+# TODO: create playlist
+
+
+# TODO: get top songs from each band
+
+
+
+find_all(name ='div', class_="image-slide-title")
